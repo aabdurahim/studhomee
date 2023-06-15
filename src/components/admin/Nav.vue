cd0a3d9c2e812bf947cff6a1adc36017f627b6d0
Aruzhan Abdurahim <100573843+aabdurahim@users.noreply.github.com>
2023-06-15 11:58:19 +0600

    Update Nav.vue

--- src/components/admin/Nav.vue
+++ src/components/admin/Nav.vue
@@ -1,11 +1,8 @@
 <template>
   <div class="bg-white flex items-center justify-between px-7 py-3 w-full overflow-hidden">
-    <div class="text-sm font-semibold text-gray-100 bg-hero-text rounded-md px-2">Admin</div>
+    <div class="text-sm font-semibold text-gray-100 bg-hero-text rounded-md px-2 sm:block hidden">Admin</div>
     <div class="flex items-center justify-between pr-24">
-      <p class="text-gray-500 text-lg border border-gray-300 rounded-md px-3 py-1 mr-3">Session: 2023</p>
-      <select name="lan" id="" class="text-gray-500 text-lg border border-gray-300 rounded-md px-3 py-1">
-        <option value="">EN</option>
-      </select>
+      <p class="text-gray-500 text-base sm:text-lg border border-gray-300 rounded-md px-1 sm:px-3 py-1 mr-3">Session: 2023</p>
       <font-awesome-icon icon="fa-regular fa-bell" class="text-gray-900 text-xl mx-3" />
       <div class="flex w-10 ml-2">
         <img src="/img/user.png" alt="">
