9edbfcd8b2f6a264e7d9b2d3d65a37b603e33584
Aruzhan Abdurahim <100573843+aabdurahim@users.noreply.github.com>
2023-06-15 11:54:25 +0600

    Update Shedule.vue

--- src/components/admin/academic/Shedule.vue
+++ src/components/admin/academic/Shedule.vue
@@ -10,14 +10,14 @@
         </div>
         <button @click="showForm()" class="text-lg font-semibold border-2 hover:border-main-text hover:bg-white hover:text-main-text text-white bg-blue-2 rounded-lg px-7">Create</button>
       </div>
-
+<!-- 
       <div v-if="profile" class="w-2/3 mx-auto flex items-center my-10">
         <select id="class-select" v-model="selectedClass" @change="handleClassSelect" class="border py-2 px-3 mr-3 border-gray-300 rounded-md">
         <option value="">Select Class</option>
         <option v-for="class1 in classes" :key="class1.id" :value="class1.id" >{{ class1.number }}</option>
       </select>
       
-      <div v-if="selectedClass">
+      <div>
         <select id="section-select" v-model="selectedSection" @change="handleSectionSelect" class="border py-2 px-3 mr-3 border-gray-300 rounded-md">
           <option value="">Select Section</option>
           <option v-for="section in sections" :key="section.id" :value="section.id" >{{ section.name }}</option>
@@ -27,7 +27,7 @@
       <div v-if="selectedSection">
         <button @click="getRoutine()" type="submit" class="btn border-2 hover:border-main-text hover:bg-white rounded-lg px-5 py-2 hover:text-main-text text-xl font-semibold bg-blue-2 text-white cursor-pointer transition ease-in-out delay-75">Get routine</button>
       </div>
-      </div>
+      </div> -->
 
       <div v-if="form">
         <div class="bg-white rounded-md my-10 pb-5">
@@ -37,72 +37,46 @@
             </div>
           <form @submit.prevent="handleSubmit" class="my-10 w-3/5 mx-auto">
 
-            <label>Class</label>
-            <select v-model="class1" class="w-full border py-2 px-3 mt-2 mb-5 border-gray-300 rounded-md">
-              <option value="">Select Class</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
-              <option value="4">Four</option>
-              <option value="5">Five</option>
-            </select>
+           
 
             <label>Section</label>
-            <select v-model="section" class="w-full border py-2 px-3 mt-2 mb-5 border-gray-300 rounded-md">
+            <select v-model="selectedSection" class="w-full border py-2 px-3 mt-2 mb-5 border-gray-300 rounded-md">
               <option value="">Select Section</option>
-              <option value="1">A</option>
-              <option value="2">B</option>
-              <option value="3">C</option>
-              <option value="4">D</option>
-              <option value="5">E</option>
+              <option v-for="section in sections" :key="section.id" :value="section.id" v-if="subject.class == selectedClass">{{ section.name }}</option>
             </select>
 
             <label>Subject</label>
-            <select v-model="subject" class="w-full border py-2 px-3 mt-2 mb-5 border-gray-300 rounded-md">
+            <select v-model="selectedSubject" class="w-full border py-2 px-3 mt-2 mb-5 border-gray-300 rounded-md">
               <option value="">Select Subject</option>
-              <option value="1">A</option>
-              <option value="2">B</option>
-              <option value="3">C</option>
-              <option value="4">D</option>
-              <option value="5">E</option>
+              <option v-for="subject in subjects" :key="subject.id" :value="subject.id">{{ subject.name }}</option>
             </select>
 
             <label>Class Room</label>
-            <select v-model="classRoom" class="w-full border py-2 px-3 mt-2 mb-5 border-gray-300 rounded-md">
+            <select v-model="selectedRoom" class="w-full border py-2 px-3 mt-2 mb-5 border-gray-300 rounded-md">
               <option value="">Select Class Room</option>
-              <option value="1">100</option>
-              <option value="2">101</option>
-              <option value="3">102</option>
-              <option value="4">103</option>
-              <option value="5">104</option>
+              <option v-for="room in rooms" :key="room.id" :value="room.id">{{ room.number }}</option>
             </select>
 
             <label>Teacher</label>
-            <select v-model="teacher" class="w-full border py-2 px-3 mt-2 mb-5 border-gray-300 rounded-md">
+            <select v-model="selectedTeacher" class="w-full border py-2 px-3 mt-2 mb-5 border-gray-300 rounded-md">
               <option value="">Select Teacher</option>
-              <option value="1">100</option>
-              <option value="2">101</option>
-              <option value="3">102</option>
-              <option value="4">103</option>
-              <option value="5">104</option>
+              <option v-for="teacher in teachers" :key="teacher.id" :value="teacher.id">{{ teacher.name }}</option>
             </select>
 
             <label>Day</label>
-            <select v-model="day" class="w-full border py-2 px-3 mt-2 mb-5 border-gray-300 rounded-md">
+            <select v-model="selectedWeekday" class="w-full border py-2 px-3 mt-2 mb-5 border-gray-300 rounded-md">
               <option value="">Select Day</option>
-              <option value="1">Monday</option>
-              <option value="2">Tuesday</option>
-              <option value="3">Wednesday</option>
-              <option value="4">Thursday</option>
-              <option value="5">Friday</option>
+              <option v-for="weekday in weekdays" :key="weekday.id" :value="weekday.id">{{ weekday.name }}</option>
             </select>
-
-            <div class="flex">
+<!--ТУТ выбор времм работает но не кореектно исправить-->
+             <div class="flex">
               <label>Start Time</label>
-              <input type="time" v-model="startTime" @change="validateTime" class="border-gray-300" />
+              <input type="time" v-model="startTime" class="border-gray-300" />
               <label>End Time</label>
-              <input type="time" v-model="endTime" @change="validateTime" />
-            </div>
+              <input type="time" v-model="endTime" />
+            </div> 
+
+
             <button type="submit" class="btn border-2 hover:border-main-text hover:bg-white rounded-lg px-20 py-2 mt-5 hover:text-main-text text-xl font-semibold bg-blue-2 text-white cursor-pointer transition ease-in-out delay-75">Submit</button>
           </form>
         </div>
@@ -116,7 +90,7 @@
         </div>
         <div class="bg-white rounded-md my-10 pb-5 mb-10">
           <div>
-            <h2 class="text-lg font-semibold p-5">Monday</h2>
+            <h2 class="text-lg font-semibold p-5"></h2>
             <hr>
             <div class="w-full p-5">
               <table class="w-full">
@@ -163,19 +137,22 @@ export default {
       profile: true,
       details: false,
       
-      selectedClass: '',
       selectedSection: '',
+      selectedTeacher: '',
+      selectedRoom: '',
+      selectedSubject: '',
+      selectedWeekday: '',
       sections: [],
-      classes: [],
-      class1: null,
-      section: null,
+      teachers: [],
+      weekdays: [],
+      subjects: [],
+      rooms: [],
 
       routines: {},
       routineId: null,
-      class1: "",
       section: "",
       subject: "",
-      classRoom: "",
+      room: "",
       teacher: "",
       weekday: "",  
       startTime: "",
@@ -186,21 +163,45 @@ export default {
     NavBar
   },
   created() {
-    fetch('http://91.201.214.131:8080/classes')
+    fetch('http://91.201.214.131:8080/sections')
         .then(response => response.json())
         .then(data => {
-          this.classes = data; 
-          console.log(this.classes)
+          this.sections = data; 
+        });
+
+        fetch('http://91.201.214.131:8080/teachers')
+        .then(response => response.json())
+        .then(data => {
+          this.teachers = data; 
+        });
+        
+        fetch('http://91.201.214.131:8080/rooms')
+        .then(response => response.json())
+        .then(data => {
+          this.rooms = data; 
+        });
+
+        fetch('http://91.201.214.131:8080/subjects')
+        .then(response => response.json())
+        .then(data => {
+          this.subjects = data; 
+        });
+
+        fetch('http://91.201.214.131:8080/routines')
+        .then(response => response.json())
+        .then(data => {
+          this.routines = data; 
         });
     },
   methods: {
     handleSubmit() {
+      
       let body = {
         teacher: {id: this.selectedTeacher},
         section: {id: this.selectedSection},
         subject: {id: this.selectedSubject},
-        classRoom: {id: this.classRoom},
-        weekday: {id: this.wwekday},
+        classRoom: {id: this.selectedRoom},
+        weekday: {id: this.selectedWeekday},
         start: "2020-01-02T20:20", 
         end: "2021-01-02T12:30"
       }
@@ -211,12 +212,22 @@ export default {
         body: JSON.stringify(
           body
       )})
+      
       .then(response => {
           console.log(response.data);
         })
         .catch(error => {
           console.error(error);
         });
+        axios
+    .post('http://91.201.214.131:8080/routines', body)
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+
     },
     handleClassSelect() {
       console.log(this.selectedClass)
@@ -257,7 +268,7 @@ export default {
         section: {id: this.selectedSection},
         subject: {id: this.selectedSubject},
         classRoom: {id: this.classRoom},
-        weekday: {id: this.wwekday},
+        weekday: {id: this.weekday},
         start: "2020-01-02T20:20", 
         end: "2021-01-02T12:30"
     })
@@ -286,7 +297,7 @@ export default {
     showForm() {
       this.form = true,
       this.profile = false,
-      this.details - false
+      this.details = false
     },
     closeForm() {
       this.form = false,
